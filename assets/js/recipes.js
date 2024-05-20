def1590b3434a750bd84dd616c0e03cc602974c7
praveen200307 <crouselearn@gmail.com>
2024-05-20 06:24:13 -0400

    FEAT: added detail page

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -68,12 +68,187 @@ $filterSubmit.addEventListener("click",function(){
       }
    }
 
-   console.log(queries);
+    window.location.href = queries.length ? `/recipes.html?${queries.join('&').replace(/,/g,'=')}`:'./recipes.html';
+
 
 });
 
+$filterSearch.addEventListener("keydown",e=>{
+  if( e.key === "Enter") $filterSubmit.click();
+})
 
-// 
+$filterClear.addEventListener('click',function(){
+  const $filterCheckBoxes = $filterBar.querySelectorAll("input:checked");
+
+  $filterCheckBoxes?.forEach(elem => elem.checked = false );
+  $filterSearch.value &&= '';
+});
+
+const queryStr = window.location.search.slice(1);
+const queries = queryStr && queryStr.split("&").map( i => i.split("="));
+const $filterCount = document.querySelector('[data-filter-count]');
+
+if(queries.length){
+   $filterCount.style.display="block";
+   $filterCount.innerHTML = queries.length;
+}else{
+  $filterCount.style.display = "none";
+}
+
+queryStr && queryStr.split("&").map(i =>{
+  if(i.split('=')[0] === 'q'){
+     $filterBar.querySelector("input[type='search']").value = i.split("=")[1].
+     replace(/%20/g," ");
+  }else{
+     $filterBar.querySelector(`[value="${i.split('=')[1].replace(/%20/g," ")}]"`).checked = true; 
+  }
+});
 
 
+const $filterBtn = document.querySelector('[data-filter-btn]');
 
+window.addEventListener("scroll",e=>{
+  $filterBtn.classList[window.scrollY >= 120 ? "add" : "remove"]("active");
+});
+
+/**
+ * Request recipes and render
+ */
+
+const $gridList = document.querySelector("[data-grid-list]");
+const $loadMore = document.querySelector("[data-load-more]");
+const defaultQueries = [
+  ["mealType" ,"breakfast"],
+  ["mealType","dinner"],
+  ["mealType","lunch"],
+  ["mealType","snack"],
+  ["mealType","teatime"],
+  ...cardQueries
+];
+
+$gridList.innerHTML = $skeletoncard.repeat(20);
+let nextPageUrl = "";
+
+const renderRecipe = data =>{
+ 
+  data.hits.map( (item,index)=>{
+      const {
+         recipe :{
+           image,
+           label:title,
+           totalTime:cookingTime,
+           uri:urival
+         }
+      } = item;
+
+      const recipeId=urival.slice(urival.lastIndexOf("_")+1)
+      const isSaved = window.localStorage.getItem(`cookkio-recipe${recipeId}`);
+
+      const  $card = document.createElement("div");
+      $card.classList.add("card");
+      $card.style.animationDelay = `${100 * index}ms`
+      $card.innerHTML = `
+            <figure class="card-media img-holder">
+              <img
+                src="${image}"
+                width="195"
+                height="195"
+                alt="${title}"
+                class="img-cover"
+                loading="lazy"
+              />
+            </figure>
+
+            <div class="card-body">
+              <h3 class="title-small">
+                <a href="./detail.html?recipe=${recipeId}" class="card-link">
+                  ${title ?? "Untitle"}
+                </a>
+              </h3>
+
+              <div class="meta-wrapper">
+                <div class="meta-item">
+                  <span
+                    class="material-symbols-outlined"
+                    aria-hidden="true"
+                    >schedule</span
+                  >
+
+                  <span class="label-medium">${getTime(cookingTime).time || "<1"} ${getTime(cookingTime).timeUnit}
+                  </span>
+                </div>
+
+                <button
+                  class="icon-btn has-state ${isSaved ? "saved" :"removed"}"
+                  aria-label="Add to save recipes"
+                  onclick="saveRecipe(this,'${recipeId}')"
+                >
+                  <span
+                    class="material-symbols-outlined bookmark-add"
+                    aria-hidden="true"
+                    >bookmark_add</span
+                  >
+                  <span
+                    class="material-symbols-outlined bookmark"
+                    aria-hidden="true"
+                    >bookmark</span
+                  >
+                </button>
+              </div>
+            </div>
+      `;
+
+      $gridList.appendChild($card);
+
+  });
+}
+
+
+let requestedBefore = true;
+
+fetchData(queries || defaultQueries,data =>{
+   
+  const { _links,_links:{next}  } = data;
+  nextPageUrl = next?.href;
+
+  $gridList.innerHTML ="";
+  requestedBefore = false;
+
+  if(data.hits.length){
+    renderRecipe(data)
+  }else{
+    $loadMore.innerHTML = `<p class="body-medium info-text">No recipet found </p>`
+  }
+
+})
+
+const CONTAINER_MAX_WIDTH = 1200;
+const CONTAINER_MAX_CARD =6;
+
+window.addEventListener("scroll", async e =>{
+  
+  if($loadMore.getBoundingClientRect().top < window.innerHeight && !requestedBefore && nextPageUrl){
+
+    $loadMore.innerHTML = $skeletoncard.repeat(Math.round(($loadMore.
+      clientWidth / (CONTAINER_MAX_WIDTH)) * CONTAINER_MAX_CARD ));
+      requestedBefore = true;
+
+      const response = await fetch(nextPageUrl);
+      const data = await response.json();
+
+      const { _links,_links:{next}  } = data;
+      nextPageUrl = next?.href;
+
+      renderRecipe(data);
+      $loadMore.innerHTML = "";
+      requestedBefore = false;
+
+  }
+
+  if(!nextPageUrl) $loadMore.innerHTML = `<p class="body-medium info-text">
+  No More recipes </p>`;
+
+  
+
+
+})
